863efffc30c6fb61666a4d2724a7844129b3748c
Yacine Sibous <yacinesibous@gmail.com>
2018-07-09 13:26:45 -0400

    juggle

--- juggle.js
+++ juggle.js
@@ -1,24 +1,20 @@
 var http = require('http');
-var urls = [process.argv[2], process.argv[3], process.argv[4]];
+var urls = process.argv.splice(2);
 var items_processed = 0;
-var body = '';
-urls.forEach((url) => { 
+var body = ['', '', ''];
+urls.forEach((url, i) => {
     http.get(url, (res) => {
-    res.on('data', (line) => {
-        body += line;
+        res.setEncoding('utf8');
+        res.on('data', (line) => {
+            body[i] += line;
+        });
+        res.on('end', () => {
+            items_processed += 1;
+            if (items_processed == urls.length) {
+                body.forEach((txt) => {
+                    console.log(txt);
+                });
+            }
+        });
     });
-    res.on('end',() => )
-    items_processed += 1;
-    if (items_processed == urls.length) {
-
-    }
-  
 });
-});
-
-function log_to_screen(body) {
-    res.on('end', () => {
-        console.log(body.length);
-        console.log(body);
-    });
-}
